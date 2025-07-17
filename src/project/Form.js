6339011d0c5effedba36a4e288d849eb86a0f5b3
mouaiz-09 <rlqyyn@gmail.com>
2025-07-17 13:25:57 +0100

    تعديل خدمة googel sheet ربط الصحيح

--- src/project/Form.js
+++ src/project/Form.js
@@ -1,5 +1,9 @@
 import { useState } from "react"
+import LoadingCircleSpinner from "./LoadingCircleSpinner";
+
 export default function Form(params) {
+    const [vewstate, setvew] = useState(true);
+
     //state for datat form
     const [FormDtat, SetFormDatat] = useState({
         Name: "",
@@ -14,7 +18,7 @@ export default function Form(params) {
         if (Name.length > 3 && Message.length > 5 && Phone.length === 10) {
             //ftech 
 
-        await    fetch("https://script.google.com/macros/s/AKfycbwdSbpzdCx9qJNWc2BtYPB8wprcXHNGWVWYnWeD-PLRKHF9He09vOpgRqx_GpxWVOM2/exec", {
+            await fetch("https://script.google.com/macros/s/AKfycbwdSbpzdCx9qJNWc2BtYPB8wprcXHNGWVWYnWeD-PLRKHF9He09vOpgRqx_GpxWVOM2/exec", {
                 method: 'POST',
                 body: JSON.stringify(FormDtat)
             })
@@ -22,12 +26,14 @@ export default function Form(params) {
                 .then(response => {
                     alert('✅ تم إرسال الرسالة بنجاح!');
                     SetFormDatat({ Name: '', Phone: '', Email: '', Message: '' });
+                    setvew(false)
                 })
                 .catch(err => {
                     console.error('❌ خطأ في الإرسال:', err);
                     alert('⚠️ صار خطأ أثناء الإرسال');
+                    setvew(false)
                 });
-         
+
 
 
 
@@ -45,6 +51,7 @@ export default function Form(params) {
             <h1 style={{ textAlign: "center" }}>أحصل على عرض </h1>
             <form onSubmit={(ev) => {
                 ev.preventDefault();
+                setvew(true)
 
 
 
@@ -80,5 +87,8 @@ export default function Form(params) {
                 </div>
             </form>
         </div>
+        <LoadingCircleSpinner statae={vewstate}  ></LoadingCircleSpinner>
+
+
     </>)
 }
