23bbf72e5509c50215b67f103305e28faa2d2585
mouaiz-09 <rlqyyn@gmail.com>
2025-07-17 11:56:51 +0100

    add googe sheet

--- src/project/Form.js
+++ src/project/Form.js
@@ -1,30 +1,80 @@
+import { useState } from "react"
 export default function Form(params) {
+    //state for datat form
+    const [FormDtat, SetFormDatat] = useState({
+        Name: "",
+        Phone: "",
+        Email: "",
+        Message: ""
+    })
+    const { Name, Phone, Email, Message } = FormDtat;
+
+    //very dtat :
+    const PushDtat = async () => {
+        if (Name.length > 3 && Message.length > 5 && Phone.length === 10) {
+            //ftech 
+
+        await    fetch("https://script.google.com/macros/s/AKfycbwdSbpzdCx9qJNWc2BtYPB8wprcXHNGWVWYnWeD-PLRKHF9He09vOpgRqx_GpxWVOM2/exec", {
+                method: 'POST',
+                body: JSON.stringify(FormDtat)
+            })
+                .then(res => res.text())
+                .then(response => {
+                    alert('✅ تم إرسال الرسالة بنجاح!');
+                    SetFormDatat({ Name: '', Phone: '', Email: '', Message: '' });
+                })
+                .catch(err => {
+                    console.error('❌ خطأ في الإرسال:', err);
+                    alert('⚠️ صار خطأ أثناء الإرسال');
+                });
+         
+
+
+
+        }
+        else {
+            alert("erro")
+        }
+
+    }
+
+
+
     return (<>
         <div className="formsection">
             <h1 style={{ textAlign: "center" }}>أحصل على عرض </h1>
             <form onSubmit={(ev) => {
-                ev.preventDefault()
+                ev.preventDefault();
+
+
+
             }}>
 
-                <div className="FormInputs"   id="Contects">
+                <div className="FormInputs" id="Contects">
                     <div>
                         <label>Full Name</label>
-                        <input type="txt"></input>
+                        <input type="txt" value={Name}
+                            onChange={(ev) => SetFormDatat({ ...FormDtat, Name: ev.target.value })} ></input>
                     </div>
                     <div>
                         <label>Phone</label>
-                        <input type="number"></input>
+                        <input type="number" value={Phone} onChange={(ev) => SetFormDatat({ ...FormDtat, Phone: ev.target.value })} ></input>
                     </div>
                     <div>
                         <label>Email</label>
-                        <input type="email"></input>
+                        <input type="email" value={Email} onChange={(ev) => SetFormDatat({ ...FormDtat, Email: ev.target.value })} ></input>
                     </div>
                     <div>
                         <label>Message</label>
-                        <textarea style={{ width: "400px", height: "50px" }}></textarea>
+                        <textarea style={{ width: "400px", height: "50px" }}
+                            value={Message}
+                            onChange={(ev) => SetFormDatat({ ...FormDtat, Message: ev.target.value })} >
+                        </textarea>
                     </div>
                     <div className="buutonHomeFace"  >
-                        <input type="submit"  value={"submit"}   className="submitinput" />
+                        <input type="submit" value={"submit"} className="submitinput"
+                            onClick={() => PushDtat()}
+                        />
 
                     </div>
                 </div>
